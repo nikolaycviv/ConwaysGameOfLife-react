6d94c0c5a4cdb29a9003da2ffff79a9005943f89
Nikolay Ivanov <nikolay9001@gmail.com>
2020-05-12 14:24:11 +0300

    initial commit npx create-react-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './App.css'
+
+function App() {
+  return (
+      <div className="App">
+          <h1>Conway's Game of Life</h1>
+      </div>
+  )
+}
+
+export default App
